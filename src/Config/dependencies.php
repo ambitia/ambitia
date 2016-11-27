960fb2234071713d14274b3888260a367f89b217
Artur Śmiarowski <artur.smiarowski@gmail.com>
2016-11-27 10:31:11 +0000

    Fix for changed namespace

--- src/Config/dependencies.php
+++ src/Config/dependencies.php
@@ -1,7 +1,7 @@
 <?php
 
 $dependencies = [
-//    \Ambitia\Contracts\Input\HttpRequestContract::class => \Ambitia\Input\Http\Symfony\Request::class,
+    \Ambitia\Contracts\Input\HttpRequestContract::class => \Ambitia\Http\Input\Symfony\Request::class,
     \Ambitia\Contracts\Routing\RouteMatcherContract::class => \Ambitia\Http\Routing\MatchRoute::class,
     \Ambitia\Contracts\Output\ResponseContract::class => \Ambitia\Http\Output\Response::class,
     \Ambitia\Contracts\Routing\DispatcherContract::class => \Ambitia\Http\Routing\FastRoute\FastRouteDispatcher::class,
